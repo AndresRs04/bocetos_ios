be1011301964c9681bde933467565b88fc8ec187
AndresRs04 <60792887+AndresRs04@users.noreply.github.com>
2024-10-14 10:25:01 -0600

    Add files via upload

---
+++ bocetos_ios-main/boceto_2_CellView/boceto_2_CellView/modelos/posts.swift
@@ -0,0 +1,16 @@
+//
+//  posts.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/9/24.
+//
+
+struct Publicacion: Codable{
+    //Decodable: Permite convertir informacion JSON a un modelo swift
+    //Encodable: Permite convertir un modelos de swift a JSON
+    //Codable: Ambas al mismo tiempo
+    var id: Int
+    var userId: Int
+    var title: String
+    var body: String
+}
