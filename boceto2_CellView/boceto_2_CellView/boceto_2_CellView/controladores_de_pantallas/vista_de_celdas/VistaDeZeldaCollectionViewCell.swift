b6e67a59d6cf3e6d77b62adf0d53eb4313791855
AndresRs04 <60792887+AndresRs04@users.noreply.github.com>
2024-10-28 10:50:42 -0600

    Add files via upload

---
+++ boceto2_CellView/boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/VistaDeZeldaCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  VistaDeZeldaCollectionViewCell.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/7/24.
+//
+
+import UIKit
+
+class VistaDeZelda: UICollectionViewCell {
+    
+    @IBOutlet weak var etiqueta: UILabel!
+    
+    @IBOutlet weak var cuerpo: UILabel!
+}
